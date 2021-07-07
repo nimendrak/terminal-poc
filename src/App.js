9a6f5416520c97f7b530ed88da4d46c096ab4bf5
NimendraK <nimendra.2019264@iit.ac.lk>
2021-07-08 00:37:28 +0530

    terminal comp regsitered

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Terminal from "react-terminal-view";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    let lines = [
+      "Hello,",
+      "My name's Amine",
+      "I'm a full-stack developer",
+      "I love everything that's related to new technology",
+      "I hope this component has been useful to you"
+    ];
+    return (
+      <div>
+        <Terminal lines={lines} />
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Terminal from 'react-terminal-view'
 
 ReactDOM.render(
   <React.StrictMode>
