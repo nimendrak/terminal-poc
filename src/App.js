3449a7704e638b52075e3de20b41e17fe777919a
NimendraK <nimendra.2019264@iit.ac.lk>
2021-07-08 02:04:45 +0530

    terminal with basic animations

--- src/App.js
+++ src/App.js
@@ -2,17 +2,37 @@ import React, { Component } from "react";
 import Terminal from "react-terminal-view";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lines: [],
+      tempArr: [],
+    };
+  }
+
+  async componentDidMount() {
+    const url =
+      "https://api.fungenerators.com/name/categories.json?start=0&limit=5";
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // this.setState({
+    //   lines: [data.contents[0][0].name],
+    // });
+
+    for (var i = 0; i < 5; i++) {
+      this.state.tempArr.push(data.contents[0][i].name);
+    }
+
+    this.setState({ lines: this.state.tempArr });
+
+    console.log(this.state.lines);
+  }
+
   render() {
-    let lines = [
-      "Hello,",
-      "My name's Amine",
-      "I'm a full-stack developer",
-      "I love everything that's related to new technology",
-      "I hope this component has been useful to you"
-    ];
     return (
       <div>
-        <Terminal lines={lines} />
+        <Terminal lines={this.state.lines} />
       </div>
     );
   }
