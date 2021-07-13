ec05aca1f36d883b3e20c8660048b9478885e8ea
NimendraK <nimendra.2019264@iit.ac.lk>
2021-07-13 13:51:39 +0530

    configured socket

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import Terminal from "react-terminal-view";
+import { io } from "socket.io-client";
+import axios from 'axios'
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       lines: [],
-      tempArr: [],
     };
   }
 
-  async componentDidMount() {
-    const url =
-      "https://api.fungenerators.com/name/categories.json?start=0&limit=3";
-    const response = await fetch(url);
-    const data = await response.json();
 
-    for (var i = 0; i < 3; i++) {
-      this.state.tempArr.push(data.contents[0][i].name);
-    }
+  useEffect(() => {
+    const socket = io.connect('http://127.0.0.1:5000/shell')
+    socket.on('connect', (data) => console.log(data))
 
-    this.setState({ lines: this.state.tempArr });
-
-    console.log(this.state.lines);
-  }
+    socket.on('newdata', (data) => console.log(data))
+  }, []);
 
   render() {
     return (
--- src/backend/controller.py
+++ src/backend/controller.py
@@ -1,45 +1,78 @@
 import os
-from flask import Flask, request, Response
+from flask import Flask, request, Response, Api
+from flask_socketio import SocketIO, send
+
+from util import make_response
 import subprocess
 
+
 app = Flask(__name__)
+app.config['SECRET KEY'] = "mykey"
+app.config["DEBUG"] = True
+api = Api(app)
+
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+def shell_run():
+    cmd = "ping www.google.lk"
+
+    process = subprocess.Popen(cmd,
+                               shell=True,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               universal_newlines=True)
+
+    while True:
+        next_line = process.stdout.readline()
+        if next_line:
+            socketio.emit('shell output', next_line)
+        elif not process.poll():
+            break
+
+    emit("newdata", {"td": sample}, namespace="/home")
+    socketio.sleep(1)
+
+@socketio.on("connect", namespace="/shell")
+def frontend_connection():
+  print("Client is Connected")
+  shell_run()
 
 
-@app.route("/api/shell", methods=["GET"])
-def run_command(cmd_and_args, print_constantly=False, cwd=None):
-    try:
-        output = []
-        process = subprocess.Popen(cmd_and_args,
-                                   shell=True,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE, cwd=cwd)
+# @app.route("/api/shell/<cmd>", methods=["GET"])
+# def run_command(cmd, print_constantly=False, cwd=None):
+#     try:
+#         output = []
+#         process = subprocess.Popen(cmd,
+#                                    shell=True,
+#                                    stdout=subprocess.PIPE,
+#                                    stderr=subprocess.PIPE,
+#                                    universal_newlines=True)
 
-        while True:
-            next_line = process.stdout.readline()
-            if next_line:
-                output.append(str(next_line))
-                if print_constantly:
-                    print(next_line)
-            elif not process.poll():
-                break
+#         while True:
+#             next_line = process.stdout.readline()
+#             if next_line:
+#                 output.append(str(next_line))
+#                 if print_constantly:
+#                     print(next_line)
+#             elif not process.poll():
+#                 break
 
-        error = process.communicate()[1]
+#         error = process.communicate()[1]
 
-        if len(output) > 0:
-            result = {}
-            result["output"] = process.returncode, '\n'.join(output), error
+#         if len(output) > 0:
+#             result = {}
+#             result["output"] = process.returncode, '\n'.join(output), error
 
-            response = make_response(result, True, 200)
-            return Response(response=response, status=200, mimetype='application/json')
-        else:
-            response = make_response("404 Error", True, 404)
-            return Response(response=response, status=200, mimetype='application/json')
+#             response = make_response(result, True, 200)
+#             return Response(response=response, status=200, mimetype='application/json')
+#         else:
+#             response = make_response("404 Error", True, 404)
+#             return Response(response=response, status=200, mimetype='application/json')
 
-
-except:
-    response = make_response('Exception', False, 404)
-    return Response(response=response, status=404, mimetype='application/json')
+#     except Exception as e:
+#         response = make_response("Exception - {}".format(e), False, 500)
+#         return Response(response=response, status=500, mimetype='application/json')
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    socketio.run(app)
