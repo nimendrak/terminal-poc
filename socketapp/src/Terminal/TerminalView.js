3cd0a5c68265199e1e2e392950df761217d71a6d
NimendraK <nimendra.2019264@iit.ac.lk>
2021-07-22 22:56:46 +0530

    ui done

--- socketapp/src/Terminal/TerminalView.js
+++ socketapp/src/Terminal/TerminalView.js
@@ -1,7 +1,6 @@
-import React from "react";
-import io from "socket.io-client";
-import Terminal from "react-terminal-view";
-// import Terminal, { ColorMode, LineType } from 'react-terminal-ui';
+import React, {useState} from "react";
+// import Terminal from "react-terminal-view";
+import Terminal, { ColorMode, LineType } from "react-terminal-ui";
 
 class TerminalView extends React.Component {
   state = {
@@ -10,37 +9,44 @@ class TerminalView extends React.Component {
   };
 
   componentWillUnmount() {
-    const {ws, interval} = this.state;
-    ws.close()
-    clearInterval(interval)
+    const { ws, interval } = this.state;
+    ws.close();
+    clearInterval(interval);
   }
 
   componentDidMount() {
-   const ws = new WebSocket('ws://localhost:8000/shell')
-    ws.onmessage = this.onMessage
+    const ws = new WebSocket("ws://localhost:8000/shell");
+    ws.onmessage = this.onMessage;
+
+    this.setState({
+      ws: ws,
+      // Create an interval to send echo messages to the server
+      interval: setInterval(() => ws.send("echo"), 1000),
+    });
 
     console.log("component mounted");
   }
 
   onMessage = (ev) => {
-    const recv = JSON.parse(ev.data)
-    console.log(recv.value)
-    this.socketData = recv.value
+    const recv = JSON.parse(ev.data);
+    console.log(recv.value);
+    this.socketData = recv.value;
     this.setState({
       logs: this.state.logs.concat(this.socketData),
     });
-  }
+  };
 
   render() {
     return (
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-        }}
-      >
-        <Terminal lines={this.state.logs} />
+      <div className="container">
+        {/* <Terminal
+          name="CloudTerra Backend Logs"
+          colorMode={ColorMode.Dark}
+          lineData={this.logs}
+          onInput={(terminalInput) =>
+            console.log(`New terminal input received: '${terminalInput}'`)
+          }
+        /> */}
       </div>
     );
   }
